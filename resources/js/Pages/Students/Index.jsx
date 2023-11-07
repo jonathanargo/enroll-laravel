cc6af28edca28ac333ccf2d78bba9ff9a0a5db37
Jonathan Argo <jonathan.argo@gmail.com>
2023-11-07 14:18:17 -0500

    Minor UI change

--- resources/js/Pages/Students/Index.jsx
+++ resources/js/Pages/Students/Index.jsx
@@ -9,7 +9,9 @@ import { useForm, Head } from '@inertiajs/react';
 export default function Index({ auth , students }) {
     return (
         <AuthenticatedLayout user={auth.user}>
-            <div>
+            <Head title="Students" />
+
+            <div className="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
                 <p>All Students:</p>
                 <div className="mt-6 bg-white shadow-sm">
                 {students.map(student => 
