a15f9e67e3d087f749077c3b2f15061f558cf065
Jonathan Argo <jonathan.argo@gmail.com>
2023-11-08 22:29:31 -0500

    Testing git branch rename

--- resources/js/Pages/Students/Index copy.jsx
+++ resources/js/Pages/Students/Index copy.jsx
@@ -56,7 +56,7 @@ function TableComponent({students}) {
 export default function Index({ auth , students }) {
     return (
         <AuthenticatedLayout user={auth.user}>
-            <Head title="Students " />
+            <Head title="Students" />
             <div className="max-w-3xl mx-auto p-4 sm:p-6 lg:p-8">
                 <TableComponent students={students}/>
             </div>
